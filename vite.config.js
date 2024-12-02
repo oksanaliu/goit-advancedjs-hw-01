ee6ff7c2483b43fc3ce4f4914b2137c897cc20ac
oksanaliu <lyuklyan4@icloud.com>
2024-12-02 15:55:49 +0100

    с

--- vite.config.js
+++ vite.config.js
@@ -3,33 +3,32 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    // Корінь проєкту — коренева папка
-    root: './',
-    // Базовий шлях для GitHub Pages
-    base: '/goit-advancedjs-hw-01/',
-    build: {
-      // Вихідна папка для збірки
-      outDir: 'dist',
-      emptyOutDir: true, // Очищення папки dist перед збіркою
-      rollupOptions: {
-        // Вхідні точки
-        input: {
-          main: './index.html', // Головна сторінка в корені
-          gallery: './src/pages/gallery.html', // Сторінка галереї
-          feedback: './src/pages/feedback.html', // Сторінка зворотного зв'язку
-        },
+export default defineConfig({
+  // Встановлюємо кореневу папку
+  root: './',
+  // Базовий шлях для GitHub Pages
+  base: '/goit-advancedjs-hw-01/',
+  build: {
+    // Вказуємо папку для збірки
+    outDir: 'dist',
+    emptyOutDir: true, // Очищення dist перед збіркою
+    sourcemap: true, // Карти для дебагу
+    rollupOptions: {
+      // Вхідні точки для Rollup
+      input: {
+        main: './index.html', // Головна сторінка
+        gallery: './src/pages/gallery.html', // Сторінка галереї
+        feedback: './src/pages/feedback.html', // Сторінка зворотного зв'язку
       },
     },
-    plugins: [
-      injectHTML(), // Ін'єкція HTML
-      FullReload(['./src/**/*.html', './src/**/*.css', './src/**/*.js']), // Автоматичне перезавантаження
-      SortCss({ sort: 'mobile-first' }), // Сортування CSS для mobile-first
-    ],
-    server: {
-      host: true, // Дозволяє доступ через локальну мережу
-      port: 3000, // Порт для локального сервера
-    },
-  };
+  },
+  plugins: [
+    injectHTML(), // Ін'єкція HTML
+    FullReload(['./src/**/*.html', './src/**/*.css', './src/**/*.js']), // Оновлення при зміні файлів
+    SortCss({ sort: 'mobile-first' }), // Mobile-first сортування CSS
+  ],
+  server: {
+    host: true, // Локальна мережа
+    port: 3000, // Порт локального сервера
+  },
 });
