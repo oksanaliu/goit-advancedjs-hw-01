71273bb390ca7cfefadf46734fb01535f5e1b7ec
oksanaliu <lyuklyan4@icloud.com>
2024-12-02 15:50:55 +0100

    с

--- vite.config.js
+++ vite.config.js
@@ -5,45 +5,31 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    // Корінь проекту (директорія src)
-    root: './src',
+    // Корінь проєкту — коренева папка
+    root: './',
     // Базовий шлях для GitHub Pages
     base: '/goit-advancedjs-hw-01/',
-    resolve: {
-      alias: {
-        '/@css': '/src/css',
-        '/@js': '/src/js',
-      },
-    },
     build: {
-      sourcemap: true, // Генерація sourcemap для дебагу
-      outDir: '../dist', // Вихідна папка збірки
-      emptyOutDir: true, // Очищення вихідної папки перед збіркою
+      // Вихідна папка для збірки
+      outDir: 'dist',
+      emptyOutDir: true, // Очищення папки dist перед збіркою
       rollupOptions: {
+        // Вхідні точки
         input: {
-          main: './src/index.html', // Головна сторінка
+          main: './index.html', // Головна сторінка в корені
           gallery: './src/pages/gallery.html', // Сторінка галереї
-          feedback: './src/pages/feedback.html', // Сторінка зворотнього зв'язку
-        },
-        output: {
-          assetFileNames: assetInfo => {
-            if (/\.css$/.test(assetInfo.name)) {
-              return 'css/[name]-[hash][extname]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-          entryFileNames: '[name].js',
+          feedback: './src/pages/feedback.html', // Сторінка зворотного зв'язку
         },
       },
     },
     plugins: [
       injectHTML(), // Ін'єкція HTML
-      FullReload(['./src/**/*.html', './src/**/*.css', './src/**/*.js']), // Автоматичне оновлення під час змін
+      FullReload(['./src/**/*.html', './src/**/*.css', './src/**/*.js']), // Автоматичне перезавантаження
       SortCss({ sort: 'mobile-first' }), // Сортування CSS для mobile-first
     ],
     server: {
-      host: true, // Доступ через локальну мережу
-      port: 3000, // Порт для запуску локального сервера
+      host: true, // Дозволяє доступ через локальну мережу
+      port: 3000, // Порт для локального сервера
     },
   };
 });
