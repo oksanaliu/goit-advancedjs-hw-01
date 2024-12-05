aa3e0fff77d9de0e12c7e9cfc8a50c647e451463
oksanaliu <lyuklyan4@icloud.com>
2024-12-05 11:16:19 +0100

    Зберігання змін перед pull

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/goit-advancedjs-hw-01/',
     root: 'src',
     build: {
       sourcemap: true,
