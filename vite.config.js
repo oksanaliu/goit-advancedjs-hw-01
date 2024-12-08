b99a59f08e62ce1d5d400c303440c1a8b6977dc4
oksanaliu <lyuklyan4@icloud.com>
2024-12-08 14:19:48 +0100

    с

--- vite.config.js
+++ vite.config.js
@@ -15,9 +15,9 @@ export default defineConfig(({ command }) => {
       sourcemap: true,
       rollupOptions: {
         input: {
-          index: './src/public/index.html',
-          gallery: './src/public/gallery.html',
-          form: './src/public/feedback.html',
+          index: './src/public/index.html', // Основний файл
+          gallery: './src/public/gallery.html', // Галерея
+          form: './src/public/feedback.html', // Форма зворотного зв'язку
         },
         output: {
           manualChunks(id) {
@@ -40,11 +40,19 @@ export default defineConfig(({ command }) => {
       }),
       viteStaticCopy({
         targets: [
-          { src: './src/img/**/*', dest: 'img' },
-          { src: 'src/css/**/*', dest: 'css' },
-          { src: 'src/js/**/*', dest: 'js' },
+          {
+            src: 'src/img/**/*', // Зображення
+            dest: 'img',
+          },
+          {
+            src: 'src/css/**/*', // CSS
+            dest: 'css',
+          },
+          {
+            src: 'src/js/**/*', // JavaScript
+            dest: 'js',
+          },
         ],
-        verbose: true,
       }),
     ],
   };
