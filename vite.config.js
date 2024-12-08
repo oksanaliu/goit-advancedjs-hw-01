d510cd392226544fc4b711631eb458c07e9c9ede
oksanaliu <lyuklyan4@icloud.com>
2024-12-08 15:32:16 +0100

    с

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,57 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/public/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
+export default defineConfig(({ command }) => ({
+  define: {
+    [command === 'serve' ? 'global' : '_global']: {},
+  },
+  base: './',
+  root: 'src',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: './src/public/index.html', // Шлях до index.html
+        gallery: './src/public/1-gallery.html', // Шлях до 1-gallery.html
+        feedback: './src/public/2-form.html', // Шлях до 2-form.html
       },
-      outDir: '../dist',
-      emptyOutDir: true,
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
-});
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/public/**/*.html']),
+    SortCss({
+      sort: 'mobile-first',
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './src/img/**/*', // Копіюємо зображення з новим шляхом
+          dest: 'img',
+        },
+        {
+          src: './src/css/**/*', // Копіюємо стилі
+          dest: 'css',
+        },
+        {
+          src: './src/js/**/*', // Копіюємо скрипти
+          dest: 'js',
+        },
+      ],
+    }),
+  ],
+}));
