78945ee1dd96a8e0047bc71515affd7dfe92d7c5
oksanaliu <lyuklyan4@icloud.com>
2024-12-08 14:53:43 +0100

    c

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,40 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
+export default defineConfig(({ command }) => ({
+  define: {
+    [command === 'serve' ? 'global' : '_global']: {},
+  },
+  base: './',
+  root: 'src',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: './public/index.html',
+        gallery: './public/gallery.html',
+        feedback: './public/feedback.html',
       },
-      outDir: '../dist',
-      emptyOutDir: true,
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
-  };
-});
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./public/**/*.html']),
+    SortCss({
+      sort: 'mobile-first',
+    }),
+  ],
+}));
