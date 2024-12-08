90bb239c01700c7c1db28df54c8aa7da2972c380
oksanaliu <lyuklyan4@icloud.com>
2024-12-08 15:11:11 +0100

    с

--- vite.config.js
+++ vite.config.js
@@ -1,53 +1,49 @@
 import { defineConfig } from 'vite';
+import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => ({
-  define: {
-    [command === 'serve' ? 'global' : '_global']: {},
-  },
-  base: '/goit-advancedjs-hw-01/',
-  root: 'src',
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        index: './src/public/index.html',
-        gallery: './src/public/gallery.html',
-        feedback: './src/public/feedback.html',
-      },
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-        entryFileNames: '[name].js',
-        assetFileNames: 'assets/[name]-[hash][extname]',
-      },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
-  plugins: [
-    injectHTML(),
-    FullReload(['./src/**/**.html']),
-    SortCss({
-      sort: 'mobile-first',
-    }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/img/**/*',
-          dest: 'assets/img',
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
         },
-        {
-          src: 'src/css/**/*',
-          dest: 'assets/css',
-        },
-      ],
-    }),
-  ],
-}));
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  };
+});
+і;
