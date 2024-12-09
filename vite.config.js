bd2c87e8b0141eb5ddc96fb6f5f096365d16d8b3
oksanaliu <lyuklyan4@icloud.com>
2024-12-09 11:42:31 +0100

    с

--- vite.config.js
+++ vite.config.js
@@ -1,56 +1,57 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    base: './',
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: {
-          index: './index.html',
-          gallery: './1-gallery.html',
-          form: './2-form.html',
-        },
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: '[name].js',
-          assetFileNames: 'assets/[name]-[hash][extname]',
-        },
+export default defineConfig(({ command }) => ({
+  define: {
+    [command === 'serve' ? 'global' : '_global']: {},
+  },
+  base: './',
+  root: 'src',
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: './public/index.html',
+        gallery: './public/1-gallery.html',
+        form: './public/2-form.html',
       },
-      outDir: '../dist',
-      emptyOutDir: true,
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/*.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-      viteStaticCopy({
-        targets: [
-          {
-            src: './img/**/*', // Шлях до зображень
-            dest: 'img', // Куди копіювати в dist
-          },
-          {
-            src: './js/**/*', // JS-файли
-            dest: 'js',
-          },
-        ],
-      }),
-    ],
-  };
-});
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./public/**/*.html']),
+    SortCss({
+      sort: 'mobile-first',
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './img/**/*',
+          dest: 'img',
+        },
+        {
+          src: './css/**/*',
+          dest: 'css',
+        },
+        {
+          src: './js/**/*',
+          dest: 'js',
+        },
+      ],
+    }),
+  ],
+}));
