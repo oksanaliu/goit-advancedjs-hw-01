e1979e9848cf4f7bd9213543d02f5e7b83f5bcc5
oksanaliu <oksanaliu@users.noreply.github.com>
2024-12-09 15:02:01 +0000

    deploy: 12e5f46059282f962602d264aa72dc9edd3d1618

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,14 +1,20 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as a}from"./assets/vendor-B2mb6eXk.js";const g=[{url:"./img/img-01.jpg",bannerUrl:"./img/banner-01.jpg",alt:"wet mountain valley",width:300,height:150},{url:"./img/img-02.jpg",bannerUrl:"./img/banner-02.jpg",alt:"body of water surrounded by trees",width:300,height:150},{url:"./img/img-03.jpg",bannerUrl:"./img/banner-03.jpg",alt:"landscape photography of mountains",width:300,height:150},{url:"./img/img-04.jpg",bannerUrl:"./img/banner-04.jpg",alt:"brown wooden dock between lavender flower field",width:300,height:150},{url:"./img/img-05.jpg",bannerUrl:"./img/banner-05.jpg",alt:"body of water surrounding with trees",width:300,height:150},{url:"./img/img-06.jpg",bannerUrl:"./img/banner-06.jpg",alt:"forest heat by sunbeam",width:300,height:150},{url:"./img/img-07.jpg",bannerUrl:"./img/banner-07.jpg",alt:"photo of valley",width:300,height:150},{url:"./img/img-08.jpg",bannerUrl:"./img/banner-08.jpg",alt:"green grass field during sunset",width:300,height:150},{url:"./img/img-09.jpg",bannerUrl:"./img/banner-09.jpg",alt:"aerial shot of forest",width:300,height:150}],i=r=>r.map(e=>`
-      <li class="gallery__item">
-        <a class="gallery__link" href="${e.bannerUrl}">
-          <img
-            class="gallery__image"
-            src="${e.url}"
-            alt="${e.alt}"
-            width="${e.width}"
-            height="${e.height}"
-          />
-        </a>
-      </li>
-      `).join(""),l=document.querySelector(".js-gallery");l.innerHTML=i(g);new a(".js-gallery a",{captionsData:"alt",captionDelay:250});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const n=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Haulage Freight"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",description:"Aerial Beach View"},{preview:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",original:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",description:"Flower Blooms"},{preview:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",original:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",description:"Alpine Mountains"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",description:"Mountain Lake Sailing"},{preview:"https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",description:"Alpine Spring Meadows"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",description:"Nature Landscape"},{preview:"https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",description:"Lighthouse Coast Sea"}],r=document.querySelector(".gallery"),d=n.map(({preview:e,original:i,description:c})=>`
+  <li class="gallery-item">
+    <a class="gallery-link" href="${i}">
+      <img
+        class="gallery-image"
+        src="${e}"
+        data-source="${i}"
+        alt="${c}"
+      />
+    </a>
+  </li>
+`).join("");r.innerHTML=d;let t=0;const s=e=>{const{original:i,description:c}=n[e];return`
+    <div class="modal-content">
+      <button class="btn-close" aria-label="Close">&times;</button>
+      <button class="btn-prev">&larr;</button>
+      <img src="${i}" alt="${c}" class="modal-image">
+      <button class="btn-next">&rarr;</button>
+    </div>
+  `};r.addEventListener("click",e=>{if(e.preventDefault(),!e.target.classList.contains("gallery-image"))return;const c=e.target.dataset.source;t=n.findIndex(o=>o.original===c),basicLightbox.create(s(t),{onShow:o=>{const a=o.element();a.querySelector(".btn-close"),a.querySelector(".btn-prev"),a.querySelector(".btn-next");const l=()=>{t=(t+1)%n.length,o.element().innerHTML=s(t),p()},g=()=>{t=(t-1+n.length)%n.length,o.element().innerHTML=s(t),p()},h=()=>{o.close()},p=()=>{a.querySelector(".btn-close").addEventListener("click",h),a.querySelector(".btn-next").addEventListener("click",l),a.querySelector(".btn-prev").addEventListener("click",g)};p()}}).show()});
 //# sourceMappingURL=1-gallery.js.map
